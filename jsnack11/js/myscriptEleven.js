2e16d6890c6818f7a94640d7580dd74b9cce67e3
Donatella7 <campaniellodonatella@gmail.com>
2022-03-03 12:14:59 +0100

    snack 11

---
+++ jsnack11/js/myscriptEleven.js
@@ -0,0 +1,22 @@
+
+const arrayNumbers = [1,2,5,7,10,23,65];
+
+function checkEvenOdd(number){
+    if((number % 2) == 0){
+        return true;
+    }
+
+    return false;
+}
+
+const divRed = document.getElementById("my-red");
+const divGreen = document.getElementById("my-green");
+
+for(let i = 0; i < arrayNumbers.length; i++){
+    
+    if(checkEvenOdd(arrayNumbers[i])){
+        divRed.innerHTML = arrayNumbers[i];
+    } else {
+        divGreen.innerHTML = arrayNumbers[i];
+    }
+}
